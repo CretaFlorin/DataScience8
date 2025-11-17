d02b52a1f22bf27f0b8a554faaf3a4d5f67bcf49
Florin Creta <cretaflorin4444@gmail.com>
2025-11-17 19:03:00 +0200

    P1

---
+++ Algo/P1.py
@@ -0,0 +1,15 @@
+'''
+https://leetcode.com/problems/regular-expression-matching/
+Se cere sa se verifice daca o expresie regex se potriveste unui text.
+Regulile regex se plaica doar pentru '.' si '*'
+
+Ex:
+
+text:  baabb
+regex: ba*aab*
+R:   True
+
+text:  babb
+regex: ba*aab*
+R:   False
+'''
